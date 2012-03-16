92ea12d819343a21712df53754384d4dd13e1aa8
Alice <herrada.aliciab@gmail.com>
2012-03-16 16:17:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//When the page is load, a message apear
+$(document).ready(function(){
+                      window.alert("The page is already load");
+                      document.getElementById("alias").focus();
+                  }
+);
+            
+            
+
+
+//document.getElementById("alias").focus();
+
+
