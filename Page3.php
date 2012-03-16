70c380da1af353f2dfa0ac8c7cfc82714729bc55
Alice <herrada.aliciab@gmail.com>
2012-03-16 16:20:21 -0300

    first commit

---
+++ Page3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title> Page 3 </title>
+    </head>
+    <body>
+        
+        <a id="anchorPage3"></a>
+        <a href="index.php#return-point"> Back to Menu </a>
+        <br>
+        <a href="Page1.php#anchorPage1"> Go to Page 1 </a>
+        <br>
+        <a href="Page1.php#anchorPage2"> Go to Page 2 </a>
+    
+    </body>
+</html>
