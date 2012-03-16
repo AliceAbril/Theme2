0d4c326f6a0a1e7869654462e1ea9848a09f1c38
Alice <herrada.aliciab@gmail.com>
2012-03-16 16:19:38 -0300

    first commit

---
+++ Page2.php
@@ -0,0 +1,24 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title> Page 2 </title>
+    </head>
+    <body>
+        
+        <a id="anchorPage2"></a>
+        <a href="index.php#return-point"> Back to Menu </a>
+        <br>
+        <a href="Page1.php#anchorPage1"> Go to Page 1 </a>
+        <br>
+        <a href="Page3.php#anchorPage3"> Go to Page 3 </a>
+        
+    </body>
+</html>
